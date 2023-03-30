8767d530ba2b8944e6a71295ebea6294770df8d8
HuuThanhdev <nguyenhuuthanh57.it@gmail.com>
2023-03-30 23:52:59 +0700

    update popup

---
+++ .history/app_20230330233204.js
@@ -0,0 +1,33 @@
+let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+
+// Popup
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// checkbox - select all
+function CheckAll(parent){
+	var ids = document.getElementsByTagName('input');
+	for(var i=0; i<ids.length; i++){
+		if(ids[i].class == "form-check-input"){
+			ids[i].checked = parent.checked;
+		}
+	}
+}
+// checkbox - select all
